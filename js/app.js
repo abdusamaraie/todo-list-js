3d6176352fa332ed8f8dab846b4914cf97f80e98
Abdu Samaraie <abdusamaraie@gmail.com>
2019-12-05 21:12:35 -0500

    Add main functionality: adding a  new todo item

--- js/app.js
+++ js/app.js
@@ -1,10 +1,23 @@
 // mark the li checked by making a strickthrow
-$('li').on('click',function(){
+$('ul').on('click','li',function(){
   $(this).toggleClass('checked');
 
 });
 //delete the hole li on click of the X
-$('.deleteMe').on('click',function(event){
-  $(this).parent().addClass('deleted');
+$('span').on('click',function(event){
+  $(this).parent().fadeOut(500,function(){
+    $(this).remove();
+  });
   event.stopPropagation();
+})
+
+//Add new todo
+$("input[type='text']").on('keypress',function(event){
+  if(event.which === 13){
+    //Grabbing the text from input
+    var todo = $(this).val();
+    $('ul').append('<li>' +"<span>X</span> "+ todo + '</li>');
+    /// clear the text input
+    $(this).val("");
+  }
 })
